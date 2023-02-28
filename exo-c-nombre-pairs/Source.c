9d0eb8e0bb74ad1db9427a2a0ca6520e74a414fd
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-02-28 16:41:38 +0100

    Add files via upload

---
+++ exo-c-nombre-pairs/Source.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+	//Afficher les nombres pairs de un a dix
+	for (int i = 0; i < 11; i++) {
+		if (i % 2 == 0) {
+			printf_s("%d", i);
+		}
+	}
+
+
+}
